7db1c8c259f8400219e4a7f5ddf10e0dc2784282
tatsatmishra31-crypto <tatsatmishra31@gmail.com>
2026-07-21 18:09:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const text = "Aadvan Palace";
 
+let index = 0;
+
+function typeEffect() {
+
+    if (index < text.length) {
+
+        document.getElementById("typing-title").innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeEffect, 180);
+
+    }
+
+}
+
+window.onload = typeEffect;
